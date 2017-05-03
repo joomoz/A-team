f4f3b9b697ecda80817a95c95a977d815de60d2f
Mikko Tianen <mikko.masala@gmail.com>
2017-05-03 16:54:52 +0300

    cucumber creation tests for all types

--- features/reference_new.feature
+++ features/reference_new.feature
@@ -1,13 +1,67 @@
 Feature: As a user I want to be able to add any reference to the system.
 
-  Scenario: User can add a manual reference to the system
-    Given User goes to a new manual page
-    And  User fills Title "Manual for life", Key "Man1"
-    When User clicks Create Manual
-    Then Page contains text "Manual was successfully created"
+    Scenario: User can add a booklet reference to the system
+        Given User goes to new booklet page
+        And  User fills Title "Booklet"
+        When User clicks Create Booklet
+        Then Page contains text "Booklet was successfully created"
 
-  Scenario: User can add a techreport reference to the system
-    Given User goes to new techreport page
-    And  User fills Author "Engineer", Title "Report", Institution "MIT", Year 1998, Key "TR2"
-    When User clicks Create Techreport
-    Then Page contains text "Techreport was successfully created"
+    Scenario: User can add a conference reference to the system
+        Given User goes to new conference page
+        And  User fills Author "Person", Title "Conference", Booktitle "Book", Year 2001
+        When User clicks Create Conference
+        Then Page contains text "Conference was successfully created"
+
+#    Scenario: User can add a inbook reference to the system
+#        Given User goes to new inbook page
+#        And  User fills Author "Author", Title "Inbook", Pages "1-23", Publisher "Publisher", Year 2001
+#        When User clicks Create Inbook
+#        Then Page contains text "Inbook was successfully created"
+
+    Scenario: User can add a incollection reference to the system
+        Given User goes to new incollection page
+        And  User fills Author "Auhtor", Title "Incollection", Booktitle "Book", Publisher "publishe", Year 1998
+        When User clicks Create Incollection
+        Then Page contains text "Incollection was successfully created"
+
+    Scenario: User can add a manual reference to the system
+        Given User goes to a new manual page
+        And  User fills Title "Manual for life", Key "Man1"
+        When User clicks Create Manual
+        Then Page contains text "Manual was successfully created"
+
+    Scenario: User can add a mastersthesis reference to the system
+        Given User goes to new mastersthesis page
+        And  User fills Author "Opiskelija", Title "Mastersthesis", School "MIT", Year 1998
+        When User clicks Create Mastersthesis
+        Then Page contains text "Mastersthesis was successfully created"
+
+    Scenario: User can add a misc reference to the system
+        Given User goes to new misc page
+        And  User fills Author "Misc", Title "Misc"
+        When User clicks Create Misc
+        Then Page contains text "Misc was successfully created"
+
+    Scenario: User can add a phdthesis reference to the system
+        Given User goes to new phdthesis page
+        And  User fills Author "Tutkija", Title "PhD", School "Helsingin yliopisto", Year 2001
+        When User clicks Create Phdthesis
+        Then Page contains text "Phdthesis was successfully created"
+
+    Scenario: User can add a proceedings reference to the system
+        Given User goes to new proceedings page
+        And  User fills Title "Proceedings", Year 2001
+        When User clicks Create Proceedings
+        Then Page contains text "Proceeding was successfully created"
+
+    Scenario: User can add a techreport reference to the system
+        Given User goes to new techreport page
+        And  User fills Author "Engineer", Title "Report", Institution "MIT", Year 1998
+        When User clicks Create Techreport
+        Then Page contains text "Techreport was successfully created"
+
+    Scenario: User can add a unpublished reference to the system
+        Given User goes to new unpublished page
+        And  User fills Author "Mystery", Title "unpublished", Note "Mystery publication"
+        When User clicks Create Unpublishded
+        Then Page contains text "Unpuplished was successfully created"
--- features/step_definitions/reference_new_steps.rb
+++ features/step_definitions/reference_new_steps.rb
@@ -39,3 +39,151 @@
   Then(/^Page contains text "([^"]*)"$/) do |text|
     expect(page).to have_content text
   end
+
+# Crazy amount of generic steps
+Given(/^User fills Title "([^"]*)"$/) do |title|
+  fill_in 'Title', :with => title
+end
+
+Given(/^User goes to new booklet page$/) do
+    visit "/"
+    click_on "New Booklet"
+end
+
+When(/^User clicks Create Booklet$/) do
+    @n = Reference.count
+    click_on 'Create Booklet'
+end
+
+Given(/^User goes to new conference page$/) do
+    visit "/"
+    click_on "New Conference"
+end
+
+Given(/^User fills Author "([^"]*)", Title "([^"]*)", Booktitle "([^"]*)", Year (\d+)$/) do |author, title, book, year|
+fill_in 'Author', :with => author
+fill_in 'Title', :with => title
+fill_in 'Booktitle', :with => book
+fill_in 'Year', :with => year
+end
+
+When(/^User clicks Create Conference$/) do
+    @n = Reference.count
+    click_on 'Create Conference'
+end
+
+Given(/^User goes to new inbook page$/) do
+    visit "/"
+    click_on "New Inbook"
+end
+
+Given(/^User fills Author "([^"]*)", Title "([^"]*)", Pages "([^"]*)", Publisher "([^"]*)", Year (\d+)$/) do |author, title, pages, publisher, year|
+    fill_in 'Author', :with => author
+    fill_in 'Title', :with => title
+    fill_in 'Pages', :with => pages
+    fill_in 'Publisher', :with => publisher
+    fill_in 'Year', :with => year
+end
+
+When(/^User clicks Create Inbook$/) do
+    @n = Reference.count
+    click_on 'Create Inbook'
+end
+
+Given(/^User goes to new incollection page$/) do
+    visit "/"
+    click_on "New Incollection"
+end
+
+Given(/^User fills Author "([^"]*)", Title "([^"]*)", Booktitle "([^"]*)", Publisher "([^"]*)", Year (\d+)$/) do |author, title, book, publisher, year|
+    fill_in 'Author', :with => author
+    fill_in 'Title', :with => title
+    fill_in 'Booktitle', :with => book
+    fill_in 'Publisher', :with => publisher
+    fill_in 'Year', :with => year
+end
+
+When(/^User clicks Create Incollection$/) do
+    @n = Reference.count
+    click_on 'Create Incollection'
+end
+
+Given(/^User goes to new mastersthesis page$/) do
+    visit "/"
+    click_on "New Masters thesis"
+end
+
+Given(/^User fills Author "([^"]*)", Title "([^"]*)", School "([^"]*)", Year (\d+)$/) do |author, title, school, year|
+    fill_in 'Author', :with => author
+    fill_in 'Title', :with => title
+    fill_in 'School', :with => school
+    fill_in 'Year', :with => year
+end
+
+When(/^User clicks Create Mastersthesis$/) do
+    @n = Reference.count
+    click_on 'Create Mastersthesis'
+end
+
+Given(/^User goes to new misc page$/) do
+    visit "/"
+    click_on "New misc"
+end
+
+Given(/^User fills Author "([^"]*)", Title "([^"]*)"$/) do |author, title|
+    fill_in 'Author', :with => author
+    fill_in 'Title', :with => title
+end
+
+When(/^User clicks Create Misc$/) do
+    @n = Reference.count
+    click_on 'Create Misc'
+end
+
+Given(/^User goes to new phdthesis page$/) do
+    visit "/"
+    click_on "New Ph.D. thesis"
+end
+
+When(/^User clicks Create Phdthesis$/) do
+    @n = Reference.count
+    click_on 'Create Phdthesis'
+end
+
+Given(/^User goes to new proceedings page$/) do
+    visit "/"
+    click_on "New Proceeding"
+end
+
+Given(/^User fills Title "([^"]*)", Year (\d+)$/) do |title, year|
+    fill_in 'Title', :with => title
+    fill_in 'Year', :with => year
+end
+
+When(/^User clicks Create Proceedings$/) do
+    @n = Reference.count
+    click_on 'Create Proceeding'
+end
+
+Given(/^User fills Author "([^"]*)", Title "([^"]*)", Institution "([^"]*)", Year (\d+)$/) do |author, title, institution, year|
+  fill_in 'Author', :with => author
+  fill_in 'Title', :with => title
+  fill_in 'Institution', :with => institution
+  fill_in 'Year', :with => year
+end
+
+Given(/^User goes to new unpublished page$/) do
+    visit "/"
+    click_on "New Unpublished"
+end
+
+Given(/^User fills Author "([^"]*)", Title "([^"]*)", Note "([^"]*)"$/) do |author, title, note|
+  fill_in 'Author', :with => author
+  fill_in 'Title', :with => title
+  fill_in 'Note', :with => note
+end
+
+When(/^User clicks Create Unpublishded$/) do
+    @n = Reference.count
+    click_on 'Create Unpuplished'
+end
